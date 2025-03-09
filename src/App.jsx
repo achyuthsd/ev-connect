6df3dbdfb49800dc4ead990c41ba72e8546d1886
Achyuth S <165375463+achyuthsd@users.noreply.github.com>
2025-03-09 20:36:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Footer from './Footer';
-import Login from './Login';
-import Selection from './Selection';
-import Booking from './Booking';
-import Nav from './Nav';
-import Signup from './Signup';
-import Home from './Home';
-import Help from './Help';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider, useAuth } from "./AuthContext";
-import ProtectedRoute from "./ProtectedRoute";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 
 const router = createBrowserRouter([
   {
     path: "/ev-connect/",
-    element: <><Nav /><Home /><Footer/></>
+    element: <><Nav /><Home /><Footer /></>,
   },
   {
     path: "/ev-connect/login",
-    element: <><Nav /><Login /><Footer/></>
+    element: <><Nav /><Login /><Footer /></>,
   },
   {
     path: "/ev-connect/selection",
-    element:  <><Nav /><Selection /><Footer/></>
-     
-    
+    element: <><Nav /><Selection /><Footer /></>,
   },
   {
     path: "/ev-connect/booking",
-    element: <><Nav /><Booking /><Footer/></>
+    element: <><Nav /><Booking /><Footer /></>,
   },
   {
     path: "/ev-connect/signup",
-    element: <><Nav /><Signup /><Footer/></>
+    element: <><Nav /><Signup /><Footer /></>,
   },
   {
     path: "/ev-connect/help",
-    element: <><Nav /><Help /><Footer/></>
+    element: <><Nav /><Help /><Footer /></>,
   },
 ]);
 
 function App() {
   return (
-    <AuthProvider> 
-    <RouterProvider router={router} />
-  </AuthProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   );
 }
 
 export default App;
+
